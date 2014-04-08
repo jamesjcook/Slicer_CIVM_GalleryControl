3c2c94f894eba030c69361e9a66559a4f749ac16
james <james@d41079a3-eaa1-4f09-8506-b324c47726bb>
2014-04-08 13:13:20 +0000

    vtkMRMLNDLibraryBuilder.h moved getsubdirs to getsubcont to get either files or direcotires with inclusion or exclusion list, added getsubdirs and getsubfiles
    git-svn-id: svn+ssh://civmxsrv2.duhs.duke.edu/Users/civmsvn/svn/repos/Slicer_CIVM_GalleryControl/trunk@2051 d41079a3-eaa1-4f09-8506-b324c47726bb

--- MRML/vtkMRMLNDLibraryBuilder.h
+++ MRML/vtkMRMLNDLibraryBuilder.h
@@ -106,7 +106,10 @@ class VTK_VTKMRMLNDLIBRARYBUILDER_MRML_EXPORT vtkMRMLNDLibraryBuilder : public v
   vtkMRMLNDLibraryNode * LibPointer;
   //vtkMRMLNDLibraryBuilder(const vtkMRMLNDLibraryBuilder&);//Not implemented
   void GetFilesInDirectory(std::vector<std::string> &, const std::string &);
+  void GetSubCont(std::vector<std::string> * , std::string ,std::vector<std::string> *, int) ;  
+  // core of get subdirs and get sub files, pass output pointer, path, exclusion list and, flag for get sub files, or get sub dirs
   void GetSubDirs(std::vector<std::string> * , std::string ) ; 
+  void GetSubFiles(std::vector<std::string> * , std::string ) ; 
   void GetSubCategoryies(std::vector<std::string> * , std::string ) ; // temporary placeholder for planning purposes.
   std::vector<std::string> &split(const std::string &, char, std::vector<std::string> &);
   std::vector<std::string> split(const std::string &, char);
